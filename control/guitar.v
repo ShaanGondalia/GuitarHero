96c36efad9ea62172f1e6e0012897f538cd4cf27
Shaan Gondalia <shaan.gondalia@duke.edu>
2021-11-30 17:51:30 -0500

    Button checking bugfix

--- control/guitar.v
+++ control/guitar.v
@@ -9,8 +9,8 @@ module guitar(mw_ir, old_strum, new_strum, buttons, intersections, update, inc, 
 
 	assign c0 = (buttons[0] ~^ intersections[0]);
 	assign c1 = (buttons[1] ~^ intersections[1]);
-	assign c2 = (buttons[2] ~^ intersections[3]);
-	assign c3 = (buttons[2] ~^ intersections[3]);
+	assign c2 = (buttons[2] ~^ intersections[2]);
+	assign c3 = (buttons[3] ~^ intersections[3]);
 
 	assign inc = c0 & c1 & c2 & c3; // 1 if all presses are correct, 0 else
 	assign update = new_strum & (~old_strum); // Update if new = 1 and old = 0
