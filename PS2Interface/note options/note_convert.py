6c2355fb4d47a1ee7865a424a7ce02ec67e4b544
Felix Jiang <felix.jiang@duke.edu>
2021-11-30 19:50:03 -0500

    better script and new singles

--- PS2Interface/note options/note_convert.py
+++ PS2Interface/note options/note_convert.py
@@ -9,13 +9,12 @@ input_txt = sys.argv[1]
 list_of_int = map(hex_to_int, list(input_txt))
 print(list_of_int)
 
+indices = [0, 0, 0, 0]
 for i in range(len(list_of_int)):
     binary_list = list(format(list_of_int[i], "04b"))
     # print(binary_list)
     for j in range(len(binary_list)):
         if(binary_list[j] == '1'):
-            out = "NOTE_POS" + str(j + 1) + "[" + str(i) + "] = " + str(i * -100) + ";"
-            print(out)
-        if(binary_list[j] == '0'):
-            out = "NOTE_POS" + str(j + 1) + "[" + str(i) + "] = 500;"
+            out = "NOTE_POS" + str(j + 1) + "[" + str(indices[j]) + "] = " + str(i * -100) + ";"
+            indices[j] = indices[j] + 1
             print(out)
